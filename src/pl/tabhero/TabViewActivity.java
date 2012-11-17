0f9e4af8ca0a85ad8240628ec6c9c9650e22beef
vesnell <alek666@gmail.com>
2012-11-17 09:53:12 +0100

    attempt to add imagebutton in tabviewactivity

--- src/pl/tabhero/TabViewActivity.java
+++ src/pl/tabhero/TabViewActivity.java
@@ -9,16 +9,24 @@ import android.os.PowerManager.WakeLock;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
  
 public class TabViewActivity extends Activity {
 	
 	private WakeLock mWakeLock = null;
+	private ImageButton btnPlus;
+	private TextView tab;
+	float scaleText;
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tabview);
+        
+        btnPlus = (ImageButton) findViewById(R.id.btnPlus);
         
         PowerManager pm = (PowerManager)getSystemService(Context.POWER_SERVICE);
         mWakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "lockScreenApp");
@@ -26,7 +34,7 @@ public class TabViewActivity extends Activity {
 
         
     	TextView head = (TextView) findViewById(R.id.performerAndTitle);
-    	TextView tab = (TextView) findViewById(R.id.tabInTabView);
+    	tab = (TextView) findViewById(R.id.tabInTabView);
     	
         Intent i = getIntent();
         Bundle extras = i.getExtras();
@@ -38,8 +46,23 @@ public class TabViewActivity extends Activity {
     	//tab.setText("Jakaś sobie tabulaturka typu\n----------------------------------------------------------------------------------------\n--------4--------3-------");
     	tab.setText(listOfSections);
     	
+    	initBtnPlusOnClick();
+    	
   
     }
+	
+	private void initBtnPlusOnClick() {
+		btnPlus.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				scaleText = tab.getTextSize();
+				scaleText += 0.5;
+				tab.setTextSize(scaleText);
+			}
+			
+		});
+	}
     
 	 public void onPause() {
 		 super.onPause();
