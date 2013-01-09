f027495afe1545cdfd13be62a40ac57325fb7a19
alek <alek666@gmail.com>
2013-01-09 13:57:02 +0100

    more changes in MyGestureDetector class

--- src/pl/tabhero/utils/MyGestureDetector.java
+++ src/pl/tabhero/utils/MyGestureDetector.java
@@ -3,13 +3,12 @@ package pl.tabhero.utils;
 import pl.tabhero.R;
 import pl.tabhero.TabHero;
 import pl.tabhero.local.FavoritesActivity;
+import pl.tabhero.local.FavoritesTitleActivity;
 import pl.tabhero.net.SearchActivity;
+import pl.tabhero.net.SearchTitleActivity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.widget.Toast;
@@ -20,11 +19,11 @@ public class MyGestureDetector extends SimpleOnGestureListener{
 	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
 	private Context context;
 	private Activity activity;
-	private static final String TABHERO = "TabHero";
-	private static final String NET_SEARCH_PERF = "net.SearchActivity";
-	private static final String NET_SEARCH_TITLE = "net.SearchTitleActivity";
-	private static final String FAV_SEARCH_PERF = "local.FavoritesActivity";
-	private static final String FAV_SEARCH_TITLE = "local.FavoritesTitleActivity";
+	private static final String TABHERO = TabHero.class.getSimpleName();
+	private static final String NET_SEARCH_PERF = SearchActivity.class.getSimpleName();
+	private static final String NET_SEARCH_TITLE = SearchTitleActivity.class.getSimpleName();
+	private static final String FAV_SEARCH_PERF = FavoritesActivity.class.getSimpleName();
+	private static final String FAV_SEARCH_TITLE = FavoritesTitleActivity.class.getSimpleName();
 	
 	public MyGestureDetector(Context context) {
 		this.context = context.getApplicationContext();
@@ -33,7 +32,7 @@ public class MyGestureDetector extends SimpleOnGestureListener{
 	
 	@Override
 	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-		String className = this.activity.getLocalClassName();
+		String className = this.activity.getClass().getSimpleName();
 		if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
 			return false;
 		if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
